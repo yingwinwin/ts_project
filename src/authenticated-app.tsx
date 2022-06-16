b558ab1f21da9365e1bd5b48dd966cf033e16d2a
yingwinwin <1245839794@qq.com>
2022-06-17 00:58:31 +0800

    feat: 用row 布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,6 +2,7 @@ import { useAuth } from "context/auth-context";
 import ProjectList from "screens/project-list";
 import { Button } from "antd";
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 // 从布局出发用gird  从内容出发用flex
 // 二维用grid   一维用flex
@@ -10,7 +11,7 @@ const AuthenticatenApp = () => {
   return (
     <Container>
       <Header>
-        <HeaderLeft>
+        <HeaderLeft gap={true} between={true}>
           <h3>项目</h3>
           <h3>用户</h3>
         </HeaderLeft>
@@ -18,33 +19,15 @@ const AuthenticatenApp = () => {
           <Button onClick={logout}>登出</Button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectList />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   );
 };
-
-const PageHeader = styled.header`
-  height: 6rem;
-`;
-
-// const Main = styled.main`
-//   height: calc(100vh - 6rem);
-// `
-
 const Container = styled.div`
-  display: grid;
-  /* grid-template-rows: 6rem calc(100vh - 6rem); */
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
+  /* display: grid;
+  grid-template-rows: 6rem 1fr 6rem; */
   height: 100vh;
 `;
 
@@ -55,23 +38,10 @@ const Header = styled.header`
   align-items: center;
   justify-content: space-between;
 `;
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
 
 const Main = styled.main`
   grid-area: main;
 `;
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-const Footer = styled.footer`
-  grid-area: footer;
-`;
-
 export default AuthenticatenApp;
